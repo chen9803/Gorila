e282d1c2944a0d40a8d70f4a92409946c21e46a1
jchou21 <jaredhappy@gmail.com>
2019-10-15 22:23:32 -0700

    added some text creation helper functions and started on splitting option paths
    currently has an issue where money isn't affected by what happens during the change, it is as if the changes are lost after exiting the if statement

--- js/game.js
+++ js/game.js
@@ -1,33 +1,24 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
-
+var createdText = false;
 // ============= (1) init ===================
 // init is the first function that is called. 
 // Initiate certain variables or objects for your scene here. 
 gameScene.init = function (){
     this.familyHealth = 5;
     this.money = 10;
+    // array for all the different messages that will be displayed in the game
     this.strings = ["you are a simple villager who is trying to feed your family, and you can farm or go hunt gorillas",
                     "at the start of each month, you can chose to farm or hung gorillas to earn money",
                     "at the end of the month, you will spend your money to buy food",
                     "this is the first month and you have to decide if you want to hunt gorillas or farm",
+                    "",
+                    "watashi wa moe",
                     "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban",
-                    "ban"
                    ];
-    this.style = { font: 'bold 20pt Arial', fill: 'white', align: 'left', wordWrap:{width: config.width, useAdvancedWrap: true}};
+    // style for all the text in the game and get it to wrap around the screen
+    this.style = { font: '20pt Arial', fill: 'white', align: 'left', wordWrap:{width: config.width, useAdvancedWrap: true}};
+    console.log(this.style);
     this.i = 0;
 //    this.timer = game.time.create(1000, false);
 };
@@ -53,7 +44,7 @@ gameScene.create = function () {
     this.bg = this.add.sprite(0, 0, 'background');
     // change the sprite origin to the top-left corner
     this.bg.setOrigin(0,0);
-    
+    this.moneyString = gameScene.add.text(0,config.height-30,"Moneyz: " + this.money,this.style);
     this.text = gameScene.add.text(0,0,this.strings[this.i], this.style);
 };
 
@@ -65,13 +56,41 @@ gameScene.update = function () {
     //check for active pointer 
     if(this.input.keyboard.checkDown(cursors.space, 5000)) {
         this.i ++;
-        console.log("spacebar down");
+        this.money += this.i;
+        console.log("i: " + this.i);
+        console.log("Moneyz: " + this.money);
+        this.moneyString.setText("Moneyz: " + this.money);
         this.text.setText(this.strings[this.i]);       
+    }
+    // get option text to show up on the correct index
+    if(this.i == 4){
+        // create the text and set it so it can be interacted with
+        var option1 = text("hunt gorillas",0,50).setInteractive();
+        var option2 = text("farm",500,50).setInteractive();
+        option1.on('pointerdown',function(pointer){
+            console.log('clicked option one');
+            this.money = 100;
+            console.log("Moneyz: " + this.money);
+            this.i ++;
+        });
+        option2.on('pointerdown',function(pointer){
+            console.log('clicked option 2');
+            this.money += 3;
+            console.log("Moneyz: " + this.money);
+            this.i ++;
+        });
     }
 
 };
 
+//create text and return the object, can take in just a string or take in a string and its position
+function text(string,x,y){
+    if(typeof x !== 'undefined' && typeof y !== 'undefined'){
+        return gameScene.add.text(x,y,string,gameScene.style);
+    }
+    return gameScene.add.text(0,config.height/2-10,string,gameScene.style);
 
+}
 
 //The maximum is inclusive and the minimum is inclusive 
 function getRandomInt(min, max) {
