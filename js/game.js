7c09742d02a29c0515196238358170321b28588b
Aidan Chen <36583747+chen9803@users.noreply.github.com>
2019-10-17 13:44:39 -0700

    reverted back to wednesday

--- js/game.js
+++ js/game.js
@@ -1,10 +1,5 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
-var snatched = ['false','false','false'];
-// extra variables
-// counter for text
-var i = 0;
-// check for options being visible
 var createdText = false;
 // ============= (1) init ===================
 // init is the first function that is called. 
@@ -12,21 +7,22 @@ var createdText = false;
 gameScene.init = function (){
     this.familyHealth = 5;
     this.money = 10;
-    this.traps = [];
     // array for all the different messages that will be displayed in the game
     this.strings = ["you are a simple villager who is trying to feed your family, and you can farm or go hunt gorillas",
                     "at the start of each month, you can chose to farm or hung gorillas to earn money",
                     "at the end of the month, you will spend your money to buy food",
                     "this is the first month and you have to decide if you want to hunt gorillas or farm",
                     "",
-                    "watashi wa moe",
+                    "Will you now buy food and water for your family?",
                     "ban",
+                    ""
                    ];
     // style for all the text in the game and get it to wrap around the screen
     this.style = { font: '20pt Arial', fill: 'white', align: 'left', wordWrap:{width: config.width, useAdvancedWrap: true}};
+    this.bigStyle = { font:"75pt Arial", fill: 'white', align:'center', wordWrap:{width: config.width, useAdvancedWrap: true}};
     console.log(this.style);
-    
-//    this.timer = game.time.create(1000, false);
+    this.i = 0;
+    //    this.timer = game.time.create(1000, false);
 };
 
 // ============ (2) preload =================
@@ -51,18 +47,10 @@ gameScene.create = function () {
     // change the sprite origin to the top-left corner
     this.bg.setOrigin(0,0);
     this.moneyString = gameScene.add.text(0,config.height-30,"Moneyz: " + this.money,this.style);
-    this.text = gameScene.add.text(0,0,this.strings[i], this.style);
-    for (let i = 0; i < 3; i++){
-        let trap = this.add.sprite(5,5,'enemy');
-        trap.depth = 2;
-        trap.setInteractive();
-        trap.on('pointerdown', function(pointer){
-            alert('snached');
-            snatched[i] = true;
-        });
-        this.traps.push(trap);
-    }
-    console.log(this.traps);
+    this.text = gameScene.add.text(0,0,this.strings[this.i], this.style);
+    this.familyHealthString = gameScene.add.text(config.width-250,config.height-30,"Family Health: " + this.familyHealth,this.style);
+    this.familyDeadString = gameScene.add.text(0,config.height-300,"Your family is dead", this.bigStyle);
+    this.familyDeadString.setVisible(false);
 };
 
 // ============ (4) update ==================
@@ -72,43 +60,41 @@ gameScene.update = function () {
     game.input.enabled = true;
     //check for active pointer 
     if(this.input.keyboard.checkDown(cursors.space, 5000)) {
-        i ++;
-        money(i);
-        console.log("i: " + i);
-        this.text.setText(this.strings[i]);       
-    }
-
-    for(let i = 0; i < 10; i++){
-        if(snatched[i]){
-            this.traps[i].destroy;
+        this.i ++;
+        this.money += this.i;
+        console.log("i: " + this.i);
+        console.log("Moneyz: " + this.money);
+        this.moneyString.setText("Moneyz: " + this.money);
+        this.text.setText(this.strings[this.i]);
+        if(this.i>=4){
+            this.familyHealth -= 1;
         }
+        
     }
-    console.log(snatched);
-
     // get option text to show up on the correct index
-    if(i == 4){
+    if(this.i == 4){
         // create the text and set it so it can be interacted with
-        createdText = true;
         var option1 = text("hunt gorillas",0,50).setInteractive();
-        // var option1 = this.add.text(x,y,string,this.style).setInteractive();
         var option2 = text("farm",500,50).setInteractive();
-        option1.on('pointerdown', function(pointer){
+        var deadFamily = ("Your family is dead",0,0);
+        //deadFamily.setOrigin: 
+        option1.on('pointerdown',function(pointer){
             console.log('clicked option one');
-            money(100);
-            i ++;
-            option1.destroy();
-            option2.destroy();
-            createdText = false;
-            console.log(createdText);
+            this.money = 100;
+            console.log("Moneyz: " + this.money);
+            this.i ++;
         });
         option2.on('pointerdown',function(pointer){
             console.log('clicked option 2');
-            money(3);
-            i ++;
-            option1.destroy();
-            option2.destroy();
-        });
+            this.money += 3;
+            console.log("Moneyz: " + this.money);
+            this.i ++;
+            
+    })}
+    if(this.familyHealth==0){
+        this.familyDeadString.setVisible(true);
     }
+
 
 };
 
@@ -118,16 +104,10 @@ function text(string,x,y){
         return gameScene.add.text(x,y,string,gameScene.style);
     }
     return gameScene.add.text(0,config.height/2-10,string,gameScene.style);
+
 }
 
-// function for adding money
-function money(sum){
-    gameScene.money += sum
-    console.log("Moniez: " + gameScene.money);
-    gameScene.moneyString.setText("Moneyz: " + gameScene.money);
-    return gameScene.money;
-}
-// The maximum is inclusive and the minimum is inclusive 
+//The maximum is inclusive and the minimum is inclusive 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
